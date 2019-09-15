6a2d6330e02bf2a9b771fab7544bc7def24ddb9d
Steve Contreras <stevcont96@gmail.com>
2019-09-14 23:31:23 -0600

    Procedimiento almacenado de cliente
    se ha creado el procedimiento almacenado para agregar un nuevo cliente a la base de datos.

--- Script/Procedimientos almacenados.sql
+++ Script/Procedimientos almacenados.sql
@@ -8,3 +8,16 @@ as
 
 insert into Repuesto(parte,carro,año,existencias)		
 		      values(@parteRep,@carroRep,@añoRep,@existenciasRep);
+
+
+
+create procedure CrearCliente(
+@Nombre1 varchar(20),
+@Nombr2 varchar(20),
+@Apellido1 varchar(20),
+@Apellido2 varchar(20),
+telefono varchar(8)
+)
+as 
+insert into Cliente(Nombre1,Nombr2,Apellido1,Apellido2,telefono)
+			values(@Nombre1,@Nombr2,@Apellido1,@Apellido2,@telefono);
