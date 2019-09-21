73ffe4252ee3639491ed49b4eb1c044f4bd3e9a8
Sergio <sergioguoz1@gmail.com>
2019-09-20 23:23:38 -0600

    Pruebas Planilla
    Pruebas de Planilla y Modificacion estructura de Clase Planilla 201503925

---
+++ Practica2_AYDS1/Practica2_AYDS1.Tests/PlanillaTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica2_AYDS1;
+using System.Collections;
+
+namespace Practica2_AYDS1.Tests
+{
+    /// <summary>
+    /// Descripción resumida de PlanillaTest
+    /// </summary>
+    [TestClass]
+    public class PlanillaTest
+    {
+        public PlanillaTest()
+        {
+            //
+            // TODO: Agregar aquí la lógica del constructor
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtiene o establece el contexto de las pruebas que proporciona
+        ///información y funcionalidad para la serie de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Atributos de prueba adicionales
+        //
+        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
+        //
+        // Use ClassInitialize para ejecutar el código antes de ejecutar la primera prueba en la clase
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup para ejecutar el código una vez ejecutadas todas las pruebas en una clase
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Usar TestInitialize para ejecutar el código antes de ejecutar cada prueba 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup para ejecutar el código una vez ejecutadas todas las pruebas
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void InsertarValoresCorrectosShouldReturnTrue()
+        {
+            ArrayList valuesTest = new ArrayList();
+            valuesTest.Add("2931974320101");
+            valuesTest.Add("Juan");
+            valuesTest.Add("Pablo");
+            valuesTest.Add("Gomez");
+            valuesTest.Add("Perez");
+            valuesTest.Add("3500");
+            Planilla planilla = new Planilla();
+            
+            Assert.IsTrue(planilla.IngresoDB(valuesTest));
+        }
+        [TestMethod]
+        public void InsertarValoresIncorrectosShouldReturnFalse()
+        {
+            ArrayList valuesTest = new ArrayList();
+            valuesTest.Add("2931974320101");
+            valuesTest.Add("Juan");
+            valuesTest.Add("Pablo");
+            valuesTest.Add("Gomez");
+            valuesTest.Add("Perez");
+            valuesTest.Add("%$");
+            Planilla planilla = new Planilla();
+
+            Assert.IsFalse(planilla.IngresoDB(valuesTest));
+        }
+
+        [TestMethod]
+        public void CadenaConexionCorrectaShouldReturnTrue()
+        {
+            String cadena = "Data Source=SERGIO\\SERGIO;Initial Catalog=Taller; Integrated Security=True";
+            Planilla p = new Planilla();
+            Assert.IsTrue(p.Conexion(cadena));
+        }
+
+        [TestMethod]
+        public void CadenaConexionIncorrectaShouldReturnFalse()
+        {
+            String cadena = "Data Source=SERGIO;Initial Catalog=Ta; Integrated Security=True";
+            Planilla p = new Planilla();
+            Assert.IsTrue(p.Conexion(cadena));
+        }
+    }
+}
--- Practica2_AYDS1/Practica2_AYDS1/Planilla.aspx.cs
+++ Practica2_AYDS1/Practica2_AYDS1/Planilla.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -14,14 +15,24 @@ namespace Practica2_AYDS1
 
         string cadena = "Data Source=SERGIO\\SERGIO;Initial Catalog=Taller; Integrated Security=True";
         SqlConnection con;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             Conexion(cadena);
         }
-        protected void Conexion(string cadenaConexion)
+        public Boolean Conexion(string cadenaConexion)
         {
-            con = new SqlConnection(cadena);
-            con.Open();
+            try
+            {
+                con = new SqlConnection(cadena);
+                con.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
         }
         protected void Limpiar()
         {
@@ -33,26 +44,39 @@ namespace Practica2_AYDS1
 
         protected void IngresarEmpleado(object sender, EventArgs e)
         {
+            ArrayList values = new ArrayList();
+            values.Add(TextBox1.Text);
+            values.Add(TextBox2.Text);
+            values.Add(TextBox3.Text);
+            values.Add(TextBox4.Text);
+            values.Add(TextBox5.Text);
+            values.Add(TextBox6.Text);
+            IngresoDB(values);
+        }
+
+        public Boolean IngresoDB(ArrayList values) {
             try
             {
                 SqlCommand cmd = new SqlCommand("dbo.CrearPlanilla", con);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.AddWithValue("@DPI", TextBox1.Text);
-                cmd.Parameters.AddWithValue("@Nombre1", TextBox2.Text);
-                cmd.Parameters.AddWithValue("@Nombre2", TextBox3.Text);
-                cmd.Parameters.AddWithValue("@Apellido1", TextBox4.Text);
-                cmd.Parameters.AddWithValue("@Apellido2", TextBox5.Text);
-                cmd.Parameters.AddWithValue("@Salario", Convert.ToInt32(TextBox6.Text));
+                cmd.Parameters.AddWithValue("@DPI", values[0]);
+                cmd.Parameters.AddWithValue("@Nombre1", values[1]);
+                cmd.Parameters.AddWithValue("@Nombre2", values[2]);
+                cmd.Parameters.AddWithValue("@Apellido1", values[3]);
+                cmd.Parameters.AddWithValue("@Apellido2", values[4]);
+                cmd.Parameters.AddWithValue("@Salario", Convert.ToInt32(values[5]));
                 cmd.ExecuteNonQuery();
                 con.Close();
                 Limpiar();
                 Console.WriteLine("Hello World");
+                return true;
             }
 
             catch (Exception ex)
             {
                 Console.WriteLine("Error al Ingresar Empleado: ", ex.Message);
+                return false;
             }
         }
     }
