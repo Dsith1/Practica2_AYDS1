57f693f2b61bae01eb598f53fac92b2a2f3fdf6a
Steve Contreras <stevcont96@gmail.com>
2019-09-20 18:21:38 -0600

    Funcionalidad Crear Clientes
    se ha agregado la funcionalidad de crear clientes y muestra una tabla de los clientes que ya esten en el sistema.

--- Script/Procedimientos almacenados.sql
+++ Script/Procedimientos almacenados.sql
@@ -10,14 +10,17 @@ insert into Repuesto(parte,carro,año,existencias)
 		      values(@parteRep,@carroRep,@añoRep,@existenciasRep);
 
 
-
 create procedure CrearCliente(
 @Nombre1 varchar(20),
 @Nombr2 varchar(20),
 @Apellido1 varchar(20),
 @Apellido2 varchar(20),
-telefono varchar(8)
+@telefono varchar(8)
 )
 as 
 insert into Cliente(Nombre1,Nombr2,Apellido1,Apellido2,telefono)
 			values(@Nombre1,@Nombr2,@Apellido1,@Apellido2,@telefono);
+
+create procedure ListarCliente
+as 
+select * from Cliente;
