e9475db2423201d91d899ad42348490b3c9f95f2
Sergio <sergioguoz1@gmail.com>
2019-09-20 22:26:28 -0600

    Procedimiento Almacenado Planilla
    Creacion del procedimiento almacenado de Planilla 201503925

--- Script/Procedimientos almacenados.sql
+++ Script/Procedimientos almacenados.sql
@@ -21,3 +21,16 @@ telefono varchar(8)
 as 
 insert into Cliente(Nombre1,Nombr2,Apellido1,Apellido2,telefono)
 			values(@Nombre1,@Nombr2,@Apellido1,@Apellido2,@telefono);
+
+
+CREATE PROCEDURE CrearPlanilla(
+@DPI varchar(20),
+@Nombre1 varchar(20),
+@Nombr2 varchar(20),
+@Apellido1 varchar(20),
+@Apellido2 varchar(20),
+@Salario int
+)
+as 
+insert into Planilla(DPI,Nombre1,Nombr2,Apellido1,Apellido2,Salario)
+			values(@DPI,@Nombre1,@Nombr2,@Apellido1,@Apellido2,@Salario);
